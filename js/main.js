d157982ed5f88ee5d997307778175be5612d5acc
Dhaval <dhavalbavariya07@gmail.com>
2022-12-04 22:44:00 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $(function() {
+//   $.scrollify({
+//     section : ".scrollable-section",
+//     easing: "easeOutExpo",
+//     scrollSpeed: 200,
+//     offset : 0,
+//     scrollbars: false,
+//     updateHash: false,
+//   });
+// });
+$( document ).ready(function() {
+    $.scrollify({
+        section : ".scrollable-section",
+        easing: "easeOutExpo",
+        scrollSpeed: 200,
+        offset : 0,
+        scrollbars: false,
+        updateHash: false,
+      });
+});
