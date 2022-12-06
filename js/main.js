677b64b6b535ad8a5ef5b997ca49d6dc4d7a2068
Dhaval <dhavalbavariya07@gmail.com>
2022-12-06 13:52:02 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -12,21 +12,39 @@ $( document ).ready(function() {
     $.scrollify({
         section : ".scrollable-section",
         easing: "easeOutExpo",
-        scrollSpeed: 200,
+        scrollSpeed: 100,
         offset : 0,
         scrollbars: false,
         updateHash: false,
+        touchScroll: false,
         afterResize: function() {
-          if ($(window).width() <= 770) {
-            $('.branding-logos ul li').removeClass('scrollable-section');
-            $('html').removeClass('scrollable-section');
-            $.scrollify.destroy();
-            $.scrollify.disable()
-          }else {
-            $.scrollify.enable()
-          }
+         
         },
       });
-      // $.scrollify.disable()
+      // if ($(window).width() <= 770) {
+      //   $('.branding-logos ul li').removeClass('scrollable-section');
+      //   $('html').removeClass('scrollable-section');
+      //   $.scrollify.destroy();
+      //   $.scrollify.disable()
+      // }else {
+      //   $.scrollify.enable()
+      // }
+      var alterClass = function() {
+        var ww = document.body.clientWidth;
+        if (ww < 768) {
+          $('section').removeClass('scrollable-section');
+          $('#for-scroll').removeClass('scrollable-section');
+          $.scrollify.disable()
+        } else if (ww >= 768) {
+          $('section').addClass('scrollable-section');
+          $('#for-scroll').addClass('scrollable-section');
+        };
+      };
+      $(window).resize(function(){
+        alterClass();
+      });
+      //Fire it when the page first loads:
+      alterClass();
+  
       
 });
